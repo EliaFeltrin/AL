fa904880fdba4e74d897a0f1754d2c325a997011
Elia Feltrin <eliafeltrin@duck.com>
2024-05-26 11:58:57 +0200

    tipi più piccoli possibili per N < 33

--- types.h
+++ types.h
@@ -1,10 +1,10 @@
 
-typedef float          Q_Type;
-typedef int            b_Type;                  //non metterlo mai unsigned
-typedef int            A_Type;
-typedef float          fx_Type;                //non metterlo mai unsigned
+typedef float           Q_Type;
+typedef char            b_Type;             //non metterlo mai unsigned
+typedef char            A_Type;
+typedef float           fx_Type;            //non metterlo mai unsigned
 typedef unsigned char   dim_Type;
-typedef float           lambda_Type;            //choose only between floatand double
+typedef float           lambda_Type;        //choose only between floatand double
 typedef float           mu_Type;
 typedef unsigned int    x_dec_Type;
 
