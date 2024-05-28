86f755bb7b4b175b07f1a7b165f2e3faf86b90ae
Elia Feltrin <eliafeltrin@duck.com>
2024-05-28 11:40:58 +0200

    new types

--- types.h
+++ types.h
@@ -1,10 +1,10 @@
 
-typedef float          Q_Type;
-typedef int          b_Type;                  //non metterlo mai unsigned
-typedef int          A_Type;
-typedef float          fx_Type;                //non metterlo mai unsigned
-typedef unsigned char   dim_Type;
+typedef float           Q_Type;
+typedef int8_t          b_Type;                  //non metterlo mai unsigned
+typedef int8_t          A_Type;
+typedef float           fx_Type;                //non metterlo mai unsigned
+typedef uint8_t         dim_Type;
 typedef float           lambda_Type;            //choose only between floatand double
 typedef float           mu_Type;
-typedef unsigned int    x_dec_Type;
+typedef uint64_t        x_dec_Type;
 
